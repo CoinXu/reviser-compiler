cc3b2f69d6a648553b5cd2f0b2ada87ccd9f4167
coinxu <duanxian0605@gmail.com>
2020-06-23 17:43:09 +0800

    tmp

--- src/main.cpp
+++ src/main.cpp
@@ -13,33 +13,29 @@
 #include <iostream>
 #include <fstream>
 
-#include <message.h>
 #include <parser.h>
-#include <compiler/code_generator.h>
-#include <compiler/javascript/javascript_generator.h>
-#include <compiler/descriptor.h>
 
 using namespace reviser::compiler;
 
 int main(int args, char** argv) {
 
   reviser::message::Message logger("main");
-  logger.Info("args: " + std::to_string(args));
+  logger.Info("args: " + to_string(args));
 
   for (int i = 0; i < args; ++i) {
-    logger.Info("args::[" +  std::to_string(i) + "] = " + argv[i]);
+    logger.Info("args::[" +  to_string(i) + "] = " + argv[i]);
   }
 
   if (args == 1) {
     return 1;
   }
 
-  std::string filename = argv[1];
+  string filename = argv[1];
 
   logger.Info("open file: " + filename);
 
   // read file
-  std::ifstream is(filename, std::ifstream::binary);
+  ifstream is(filename, ifstream::binary);
 
   if (!is) {
     logger.Runtime("not found file :" + filename);
@@ -55,13 +51,17 @@ int main(int args, char** argv) {
   is.close();
 
   // 转为string，传给Tokenizer
-  std::string input(buffer, length);
+  string input(buffer, length);
+
+  JavaScriptGenerator javascript;
+  TypeScriptGenerator typescript;
+  CodeGenerator generator;
 
   Tokenizer tokenizer(input);
-  JavaScriptGenerator javascript;
   Descriptor descriptor;
 
   Parser parser(&tokenizer, &javascript, &descriptor, JavaScript);
+  // Parser parser(&tokenizer, &typescript, &descriptor, TypeScript);
 
   parser.Program();
 
